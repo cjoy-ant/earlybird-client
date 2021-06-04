c119187127adaee025e98c38a1413398f3a0327b
Christine Antonio <christinejm.antonio@gmail.com>
2021-06-03 17:06:23 -0700

    home page

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -4,7 +4,8 @@ import "./Home.css";
 export default function Home() {
   return (
     <div className="Home">
-      Welcome to EarlyBird! Go to the About page to learn more about this app.
+      Welcome to <span class="bold">EarlyBird!</span> Go to the About page to
+      learn more about this app.
     </div>
   );
 }
