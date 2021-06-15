496a7c684e43c6310c5c41596478b93fc0e698cd
Christine Antonio <christinejm.antonio@gmail.com>
2021-06-14 17:58:21 -0700

    config.js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,7 @@
 const config = {
-  API_ENDPOINT: process.env.REACT_APP_API_ENDPOINT,
+  API_ENDPOINT:
+    process.env.REACT_APP_API_ENDPOINT ||
+    "https://hidden-atoll-11404.herokuapp.com/api",
 };
 
 export default config;
